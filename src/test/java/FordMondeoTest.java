b22edb6bb535ec742242c1862b64e1e0f38f2828
Jack <jaydow3@gmail.com>
2016-03-15 16:20:03 +0000

    added skeleton tests for FordMondeo class

---
+++ src/test/java/FordMondeoTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FordMondeoTest {
+
+    @Test
+    public void testCost() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDescription() throws Exception {
+
+    }
+}
